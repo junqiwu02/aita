780135926cf9a4a51c293fc12e92d9091ff6cc72
Junqi <junqiwu02@gmail.com>
2024-05-29 19:24:43 -0400

    working test groq generation with server action

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-TODO:
+## TODO:
 - [ ] Finish the README :D
 - [ ] Use URL params for modal opening and closing, and enable mobile users to swipe back to close
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,21 @@
 "use server";
 
+import Groq from "groq-sdk";
+
+const groq = new Groq({
+  apiKey: process.env.GROQ_API_KEY,
+});
+
 export async function generate(formData: FormData) {
-  console.log(formData);
+  const title = formData.get('title');
+  const chat = await groq.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: `Generate a Reddit story in the form of a r/AmItheAsshole post with the title ${title}. Include the title as the first line of the response.`,
+      },
+    ],
+    model: "llama3-8b-8192",
+  });
+  console.log(chat.choices[0]?.message?.content || "");
 }
