357a2dcfdc8a2af2b34623e0c1078a14a92227fc
Junqi <junqiwu02@gmail.com>
2024-07-07 17:40:13 -0400

    working in progress decode proof of concept

--- app/lib/hooks.ts
+++ app/lib/hooks.ts
@@ -2,7 +2,11 @@ import { FFmpeg } from "@ffmpeg/ffmpeg";
 import { fetchFile, toBlobURL } from "@ffmpeg/util";
 import { useCallback, useEffect, useRef, useState } from "react";
 
-export function useFFmpeg(id: string, title: string, titleDuration: number): [boolean, number, string, (() => Promise<void>)] {
+export function useFFmpeg(
+  id: string,
+  title: string,
+  titleDuration: number,
+): [boolean, number, string, () => Promise<void>] {
   const [rendering, setRendering] = useState(false);
   const [percentage, setPercentage] = useState(0);
   const [resURL, setResURL] = useState("");
@@ -49,7 +53,10 @@ export function useFFmpeg(id: string, title: string, titleDuration: number): [bo
       setPercentage(Math.floor(progress * 100));
     });
 
-    await ffmpeg.writeFile("title-card.png", await fetchFile("/title-card.png"));
+    await ffmpeg.writeFile(
+      "title-card.png",
+      await fetchFile("/title-card.png"),
+    );
     await ffmpeg.writeFile(
       "tmp/font.ttf",
       await fetchFile("/Montserrat-ExtraBold.ttf"),
@@ -63,26 +70,26 @@ export function useFFmpeg(id: string, title: string, titleDuration: number): [bo
       "title-card.png",
       "-filter_complex",
       "[1][0]scale2ref[title][video];" + // scale title to video
-      `[video][title]overlay=0:0:enable='lt(t,${titleDuration})'[titled];` + // overlay for titleDuration seconds
-      `[titled]drawtext=text='${title}':` + // title as drawtext since subs don't have easy customization of line and vertical spacing
-      "fontfile=/tmp/font.ttf:" +
-      "fontsize=20:" + 
-      "y=(h-text_h)/2+15:" + 
-      "x=50:" +
-      `enable='lt(t,${titleDuration})'[sub1];` +
-      "[sub1]subtitles=subs.srt:" + // body subs
-      "fontsdir=/tmp:" +
-      "force_style='Fontname=Montserrat ExtraBold,Alignment=10'",
+        `[video][title]overlay=0:0:enable='lt(t,${titleDuration})'[titled];` + // overlay for titleDuration seconds
+        `[titled]drawtext=text='${title}':` + // title as drawtext since subs don't have easy customization of line and vertical spacing
+        "fontfile=/tmp/font.ttf:" +
+        "fontsize=20:" +
+        "y=(h-text_h)/2+15:" +
+        "x=50:" +
+        `enable='lt(t,${titleDuration})'[sub1];` +
+        "[sub1]subtitles=subs.srt:" + // body subs
+        "fontsdir=/tmp:" +
+        "force_style='Fontname=Montserrat ExtraBold,Alignment=10'",
       "-preset",
       "ultrafast",
-      "output.mp4"
+      "output.mp4",
     ]);
 
     const data = await ffmpeg.readFile("output.mp4");
     setResURL(URL.createObjectURL(new Blob([data], { type: "video/mp4" })));
 
     setRendering(false);
-  }
+  };
 
   useEffect(() => {
     return () => {
@@ -104,8 +111,8 @@ export function useTransformers() {
   useEffect(() => {
     if (!worker.current) {
       // Create the worker if it does not yet exist.
-      worker.current = new Worker(new URL('./worker.js', import.meta.url), {
-        type: 'module'
+      worker.current = new Worker(new URL("./worker.js", import.meta.url), {
+        type: "module",
       });
     }
 
@@ -113,39 +120,38 @@ export function useTransformers() {
     const onMessageReceived = (e: MessageEvent) => {
       console.log(`Received message from worker: ${e.data.status}`);
       switch (e.data.status) {
-        case 'initiate':
+        case "initiate":
           console.log(`Intitiated`);
           break;
-        case 'ready':
-          console.log(`Ready`)
+        case "ready":
+          console.log(`Ready`);
           break;
-        case 'complete':
+        case "update":
+          console.log(`Received data: ${JSON.stringify(e.data)}`);
+          break;
+        case "complete":
           console.log(`Complete: ${JSON.stringify(e.data)}`);
           break;
       }
     };
 
     // Attach the callback function as an event listener.
-    worker.current.addEventListener('message', onMessageReceived);
+    worker.current.addEventListener("message", onMessageReceived);
 
     // Define a cleanup function for when the component is unmounted.
-    return () => worker.current?.removeEventListener('message', onMessageReceived);
+    return () =>
+      worker.current?.removeEventListener("message", onMessageReceived);
   });
 
   const transcribe = useCallback(async (text: string) => {
     if (worker.current) {
-      console.log('posting message');
-
-    // webworkers do not support AudioContext, so we must decode the audio file first in the main thread
-    const audioCTX = new window.AudioContext({
-        sampleRate: 16000
-    });
-    const arrayBuffer = await (await fetch(text)).arrayBuffer();
-    const decoded = await audioCTX.decodeAudioData(arrayBuffer);
-    console.log(`NUM CHANNELS: ${decoded.numberOfChannels}`);
-    const audio = decoded.getChannelData(0);
-
-    console.log(`AUDIO DATA: ${audio}`);
+      // webworkers do not support AudioContext, so we must decode the audio file first in the main thread
+      const audioCTX = new window.AudioContext({
+        sampleRate: 16000,
+      });
+      const arrayBuffer = await (await fetch(text)).arrayBuffer();
+      const decoded = await audioCTX.decodeAudioData(arrayBuffer);
+      const audio = decoded.getChannelData(0);
 
       worker.current.postMessage({ audio });
     }
--- app/lib/worker.js
+++ app/lib/worker.js
@@ -1,3 +1,5 @@
+// see https://github.com/xenova/whisper-web
+
 import { pipeline, env } from "@xenova/transformers";
 
 // Skip local model check
@@ -5,47 +7,100 @@ env.allowLocalModels = false;
 
 // Use the Singleton pattern to enable lazy construction of the pipeline.
 class PipelineSingleton {
-    static instance = null;
+  static instance = null;
 
-    static async getInstance(progress_callback = null) {
-        if (this.instance === null) {
-            this.instance = pipeline('automatic-speech-recognition', 'Xenova/whisper-tiny.en', { progress_callback });
-        }
-        return this.instance;
+  static async getInstance(progress_callback = null) {
+    if (this.instance === null) {
+      this.instance = pipeline(
+        "automatic-speech-recognition",
+        "Xenova/whisper-tiny.en",
+        { progress_callback },
+      );
     }
+    return this.instance;
+  }
 }
 
 // Listen for messages from the main thread
-self.addEventListener('message', async (event) => {
-    console.log('received message');
-    // Retrieve the classification pipeline. When called for the first time,
-    // this will load the pipeline and save it for future use.
-    const whisper = await PipelineSingleton.getInstance(x => {
-        // We also add a progress callback to the pipeline so that we can
-        // track model loading.
-        self.postMessage(x);
-    });
+self.addEventListener("message", async (event) => {
+  // Retrieve the classification pipeline. When called for the first time,
+  // this will load the pipeline and save it for future use.
+  const transcriber = await PipelineSingleton.getInstance((x) => {
+    // We also add a progress callback to the pipeline so that we can
+    // track model loading.
+    self.postMessage(x);
+  });
 
-    function callback_function(item) {
-        // console.log(`Callback: ${item}`);
+  const time_precision =
+    transcriber.processor.feature_extractor.config.chunk_length /
+    transcriber.model.config.max_source_positions;
+
+  // Storage for chunks to be processed. Initialise with an empty chunk.
+  let chunks_to_process = [
+    {
+      tokens: [],
+      finalised: false,
+    },
+  ];
+
+  // TODO: Storage for fully-processed and merged chunks
+  // let decoded_chunks = [];
+
+  function chunk_callback(chunk) {
+    let last = chunks_to_process[chunks_to_process.length - 1];
+
+    // Overwrite last chunk with new info
+    Object.assign(last, chunk);
+    last.finalised = true;
+
+    // Create an empty chunk after, if it not the last chunk
+    if (!chunk.is_last) {
+      chunks_to_process.push({
+        tokens: [],
+        finalised: false,
+      });
     }
+  }
 
-    function chunk_callback(item) {
-        console.log(`Chunk callback: ${item}}`);
-    }
+  // Inject custom callback function to handle merging of chunks
+  function callback_function(item) {
+    let last = chunks_to_process[chunks_to_process.length - 1];
 
-    const output = await whisper(event.data.audio, {
-        return_timestamps: 'word', // word level timestamps
-        chunk_length_s: 30, // for audios longer than 30s
-        stride_length_s: 5,
+    // Update tokens of last chunk
+    last.tokens = [...item[0].output_token_ids];
 
-        // Callback functions
-        // callback_function: callback_function, // after each generation step
-        chunk_callback: chunk_callback, // after each chunk is processed
-    });
+    // Merge text chunks
+    // TODO optimise so we don't have to decode all chunks every time
+    // TODO fix word level timestamp decoding for unfinished chunks
+    // let data = transcriber.tokenizer._decode_asr(chunks_to_process, {
+    // For now exclude the last unfinished chunk from the decoder
+    let data = transcriber.tokenizer._decode_asr(
+      chunks_to_process.slice(0, -1),
+      {
+        time_precision: time_precision,
+        return_timestamps: "word",
+        force_full_sequences: false,
+      },
+    );
 
     self.postMessage({
-        status: 'complete',
-        output: output,
+      status: "update",
+      data: data,
     });
+  }
+
+  const output = await transcriber(event.data.audio, {
+    return_timestamps: "word", // word level timestamps
+    chunk_length_s: 30, // for audios longer than 30s
+    stride_length_s: 5,
+
+    // Callback functions during processing
+    callback_function: callback_function,
+    chunk_callback: chunk_callback,
+  });
+
+  self.postMessage({
+    status: "complete",
+    output: output,
+  });
 });
